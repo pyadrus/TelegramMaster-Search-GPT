4d3b6e68a113fa014d99a3d7bb4364c016d07949
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-03-13 14:52:34 +0300

    Перенос данных: api_id, api_hash, username в отдельный файл config.ini. Структурирование получаемых данных по ключевому слову.

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
 # -*- coding: utf-8 -*-
 from telethon import functions
 from telethon.sync import TelegramClient
+import configparser
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+api_id = config['telegram_settings']['api_id']
+api_hash = config['telegram_settings']['api_hash']
+username = config['telegram_settings']['username']
 
 
-api_id = 1234
-api_hash = '1234'
+client = TelegramClient(username, int(api_id), api_hash)
 
-phone = +79612539374
-username = '79612539374'
-
-client = TelegramClient(username, api_id, api_hash)
 
 async def main():
     await client.connect()
@@ -19,8 +21,14 @@ async def main():
         q=search,
         limit=100
     ))
-    print(result.stringify())
-
+    for chat in result.chats:
+        print(f"ID группы / канала: {chat.id}")
+        print(f"Название группы / канала: {chat.title}")
+        print(f'Количество участников группы / канала: {chat.participants_count}')
+        print(f"Username группы / канала: {chat.username}")
+        print(f'Hash группы / канала: {chat.access_hash}')
+        print(f"Дата создания группы / канала: {chat.date}")
+        print("------------------------------")
 
 with client:
     client.loop.run_until_complete(main())
