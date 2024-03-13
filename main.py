0ffff4a1ad14ecbde0b82341b2d71f23f75d59b9
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-03-13 17:17:49 +0300

    Создание requirements

--- main.py
+++ main.py
@@ -15,26 +15,34 @@ client = TelegramClient(username, int(api_id), api_hash)
 
 async def main():
     await client.connect()
+    grup_set = set()
     list_search = ['linux', 'Линукс', 'Ubuntu', 'Debian', 'Fedora', 'ArchLinux', 'CentOS', 'Kali Linux', 'Bash',
-                   'Shell',
-                   'Kernel', 'Systemd', 'Open Source', 'GNU/Linux', 'Terminal', 'Command Line', 'Sysadmin', 'DevOps',
-                   'Server Administration', 'Linux Academy', 'Linux Education', 'Linux Users', 'Linux Community',
-                   'Linux News', 'Linux Development', 'Linux Gaming', 'Embedded Linux', 'Raspberry Pi', 'Docker',
-                   'Kubernetes', 'System Administration', 'Linux Mint', 'Elementary OS', 'LXDE', 'XFCE']
-
+                   'Shell', 'Kernel', 'Systemd', 'Open Source', 'GNU/Linux', 'Terminal', 'Command Line', 'Sysadmin',
+                   'DevOps', 'Server Administration', 'Linux Academy', 'Linux Education', 'Linux Users',
+                   'Linux Community', 'Linux News', 'Linux Development', 'Linux Gaming', 'Embedded Linux',
+                   'Raspberry Pi', 'Docker', 'Kubernetes', 'System Administration', 'Linux Mint', 'Elementary OS',
+                   'LXDE', 'XFCE']
     for search in list_search:
-        # search = 'linux'
         result = await client(functions.contacts.SearchRequest(
             q=search,
             limit=100
         ))
         for chat in result.chats:
-            print(f'ID группы / канала: {chat.id}, '
-                  f'Название группы / канала: {chat.title}, '
-                  f'Количество участников группы / канала: {chat.participants_count}, '
-                  f'Username группы / канала: {chat.username}, '
-                  f'Hash группы / канала: {chat.access_hash}, '
-                  f'Дата создания группы / канала: {chat.date}')
+            res = (f'ID группы / канала: {chat.id}, '
+                   f'Название группы / канала: {chat.title}, '
+                   f'Количество участников группы / канала: {chat.participants_count}, '
+                   f'Username группы / канала: {chat.username}, '
+                   f'Hash группы / канала: {chat.access_hash}, '
+                   f'Дата создания группы / канала: {chat.date}')
+
+            grup_set.add(res)
+
+    # Преобразование set обратно в список, если это необходимо
+    grup_list = list(grup_set)
+    # print(grup_list)
+
+    for i in grup_list:
+        print(i)
 
 with client:
     client.loop.run_until_complete(main())
