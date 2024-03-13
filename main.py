f7155ab759d64129eca6ab60acc1f0f2721318b1
@PyAdminRU <zh.vitaliy92@gmail.com>
2024-03-13 16:37:57 +0300

    Добавлен перебор по ключевым словам

--- main.py
+++ main.py
@@ -15,20 +15,26 @@ client = TelegramClient(username, int(api_id), api_hash)
 
 async def main():
     await client.connect()
+    list_search = ['linux', 'Линукс', 'Ubuntu', 'Debian', 'Fedora', 'ArchLinux', 'CentOS', 'Kali Linux', 'Bash',
+                   'Shell',
+                   'Kernel', 'Systemd', 'Open Source', 'GNU/Linux', 'Terminal', 'Command Line', 'Sysadmin', 'DevOps',
+                   'Server Administration', 'Linux Academy', 'Linux Education', 'Linux Users', 'Linux Community',
+                   'Linux News', 'Linux Development', 'Linux Gaming', 'Embedded Linux', 'Raspberry Pi', 'Docker',
+                   'Kubernetes', 'System Administration', 'Linux Mint', 'Elementary OS', 'LXDE', 'XFCE']
 
-    search = 'linux'
-    result = await client(functions.contacts.SearchRequest(
-        q=search,
-        limit=100
-    ))
-    for chat in result.chats:
-        print(f"ID группы / канала: {chat.id}")
-        print(f"Название группы / канала: {chat.title}")
-        print(f'Количество участников группы / канала: {chat.participants_count}')
-        print(f"Username группы / канала: {chat.username}")
-        print(f'Hash группы / канала: {chat.access_hash}')
-        print(f"Дата создания группы / канала: {chat.date}")
-        print("------------------------------")
+    for search in list_search:
+        # search = 'linux'
+        result = await client(functions.contacts.SearchRequest(
+            q=search,
+            limit=100
+        ))
+        for chat in result.chats:
+            print(f'ID группы / канала: {chat.id}, '
+                  f'Название группы / канала: {chat.title}, '
+                  f'Количество участников группы / канала: {chat.participants_count}, '
+                  f'Username группы / канала: {chat.username}, '
+                  f'Hash группы / канала: {chat.access_hash}, '
+                  f'Дата создания группы / канала: {chat.date}')
 
 with client:
     client.loop.run_until_complete(main())
