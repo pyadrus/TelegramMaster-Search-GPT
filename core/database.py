16204ba5ae9dff0e752da136ffce53a18ff5c417
PyAdminRU <zh.vitaliy92@gmail.com>
2025-03-16 23:32:39 +0300

    Рефакторинг кода

--- core/database.py
+++ core/database.py
@@ -3,15 +3,20 @@ import sqlite3
 
 from loguru import logger
 
+path_database = 'user_data/your_database.db'
+
 
 def save_to_database(data_tuple):
     try:
-        with sqlite3.connect('user_data/your_database.db') as conn:
+        with sqlite3.connect(path_database) as conn:
             cursor = conn.cursor()
             # Создание таблицы, если её нет
-            cursor.execute('''CREATE TABLE IF NOT EXISTS groups (id, title, participants_count, username, access_hash, date)''')
+            cursor.execute('''CREATE TABLE IF NOT EXISTS groups (id, title, participants_count, username, access_hash, 
+                              date)''')
             # Вставка данных в таблицу
-            cursor.execute('''INSERT INTO groups (id, title, participants_count, username, access_hash, date) VALUES (?, ?, ?, ?, ?, ?) ''', data_tuple)
+            cursor.execute('''INSERT INTO groups (id, title, participants_count, username, access_hash, date) 
+                                  VALUES (?, ?, ?, ?, ?, ?) ''',
+                           data_tuple)
             # Сохранение изменений и закрытие соединения
             conn.commit()
     except Exception as e:
@@ -20,14 +25,16 @@ def save_to_database(data_tuple):
 
 def remove_duplicates():
     try:
-        with sqlite3.connect('user_data/your_database.db') as conn:
+        with sqlite3.connect(path_database) as conn:
             cursor = conn.cursor()
             # Создание временной таблицы с уникальными записями
-            cursor.execute('''CREATE TEMPORARY TABLE temp_groups AS SELECT DISTINCT id, title, participants_count, username, access_hash, date FROM groups''')
+            cursor.execute('''CREATE TEMPORARY TABLE temp_groups AS SELECT DISTINCT id, title, participants_count, 
+                              username, access_hash, date FROM groups''')
             # Очистка основной таблицы
             cursor.execute('DELETE FROM groups')
             # Копирование уникальных записей обратно в основную таблицу
-            cursor.execute('''INSERT INTO groups (id, title, participants_count, username, access_hash, date) SELECT id, title, participants_count, username, access_hash, date FROM temp_groups''')
+            cursor.execute('''INSERT INTO groups (id, title, participants_count, username, access_hash, date) SELECT 
+                              id, title, participants_count, username, access_hash, date FROM temp_groups''')
             # Удаление временной таблицы
             cursor.execute('DROP TABLE temp_groups')
             # Сохранение изменений и закрытие соединения
